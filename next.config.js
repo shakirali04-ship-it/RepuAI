a9585c6e7c4efff3dc5fa6ea2e45d5a3b7031706
shakirali04-ship-it <shakir.ali.04@gmail.com>
2026-05-15 01:30:10 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
-  },
-}
-
-module.exports = nextConfig
